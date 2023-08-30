b993c20684e1f520bbe72406c5476019cb056257
WenYHsieh <menlen6205@gmail.com>
2023-08-30 14:41:31 +0800

    add use EIP-1559 for gas fee

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,40 @@ import { useEffect, useRef, useState } from 'react'
 import { ethers } from 'ethers'
 import { JsonRpcSigner } from 'ethers'
 import { TransactionResponse } from 'ethers'
+import { FeeData } from 'ethers'
 
 function App() {
   const toAddress = useRef<HTMLInputElement | null>(null)
   const [nonce, setNonce] = useState('')
   const [tx, setTx] = useState<TransactionResponse | null>(null)
   const [signer, setSigner] = useState<JsonRpcSigner | null>(null)
+  const [feeData, setFeeData] = useState<
+    Pick<FeeData, 'maxFeePerGas' | 'maxPriorityFeePerGas'>
+  >({
+    maxFeePerGas: null,
+    maxPriorityFeePerGas: null,
+  })
 
   const sendTransaction = async () => {
     if (!signer) return console.log('please connect to metamask account')
-    if (toAddress.current === null)
+    if (!toAddress.current || !nonce)
       return console.log('please enter To address')
 
-    const transaction = {
-      to: toAddress.current.value,
-      value: ethers.parseEther('0.1'), // Example value: 0.1 ETH
-      nonce: parseInt(nonce),
-    }
+    try {
+      const transaction = {
+        to: toAddress.current.value,
+        value: ethers.parseEther('0.1'),
+        nonce: parseInt(nonce),
+        ...feeData,
+        gasPrice: null,
+      }
 
-    const tx = await signer.sendTransaction(transaction)
-    await tx.wait()
-    setTx(tx)
+      const tx = await signer.sendTransaction(transaction)
+      await tx.wait()
+      setTx(tx)
+    } catch (error) {
+      console.error('Error occurred when sending transaction:', error)
+    }
   }
 
   useEffect(() => {
@@ -31,11 +44,14 @@ function App() {
       let provider
       if (window.ethereum == null) {
         console.log('MetaMask not installed; using read-only defaults')
+        provider = ethers.getDefaultProvider('HTTP://127.0.0.1:7545') // ganache
       } else {
         provider = new ethers.BrowserProvider(window.ethereum)
 
         const connectedSigner = await provider.getSigner()
         const nextNonce = await connectedSigner.getNonce()
+        const currentFeeData = await provider.getFeeData()
+        setFeeData(currentFeeData)
         setSigner(connectedSigner)
         setNonce(nextNonce.toString())
       }
